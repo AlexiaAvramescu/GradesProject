18fb8c82471f6427cd70c9fdc019fbf8effe3444
AlexTR22 <135030935+AlexTR22@users.noreply.github.com>
2025-03-25 14:24:13 +0200

    KEY-17 added function to handle the login

--- CatalogFrontend/catalog-frontend/src/pages/Login.js
+++ CatalogFrontend/catalog-frontend/src/pages/Login.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+import BASE_URL from "../services/api"; 
 import "../css/Login.css";
 
 function Login() {
@@ -6,11 +8,31 @@ function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [isProfessor, setIsProfessor] = useState(false);
+    
+    const [message, setMessage] = useState("");
 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try 
+        {
+            const response = await axios.post(`${BASE_URL}/auth/login`, {
+                username,
+                email,
+                password
+            });
+
+            setMessage(response.data.message);
+            localStorage.setItem("token", response.data.token);
+        } 
+        catch (error) {
+            setMessage("Eroare la autentificare: " + (error.response?.data.message || "Server error"));
+            alert(message);
+        }
+    };
 
     return (
         <div className="login-container">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleLogin}>
                 <h1>Login</h1>
                 <input
                     type="text"
