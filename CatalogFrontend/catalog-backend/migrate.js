1567c56b07061f9d14c72973707979c15eee6b38
AlexTR22 <135030935+AlexTR22@users.noreply.github.com>
2025-03-30 18:55:47 +0300

    KEY-19 Setup connection to docker, implement Student model, setup migrate.js file to migrate all the models to docker database

---
+++ CatalogFrontend/catalog-backend/migrate.js
@@ -0,0 +1,14 @@
+const sequelize = require("./db");
+const Student = require("./models/Student");
+
+(async () => {
+  try {
+    console.log("🔄 Încep sincronizarea bazei de date...");
+    await sequelize.sync({ alter: true }); 
+    console.log("Tabelele au fost create sau verificate cu succes.");
+    process.exit();
+  } catch (error) {
+    console.error(" Eroare la sincronizare:", error);
+    process.exit(1);
+  }
+})();
---
+++ CatalogFrontend/catalog-backend/models/Student.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+
+const Student = sequelize.define('Student', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+
+module.exports = Student;
