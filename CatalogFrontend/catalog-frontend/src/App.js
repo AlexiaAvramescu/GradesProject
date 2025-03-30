098c341c20aa6c1172d5a1a20fa3185ac2a9b2ee
AlexiaAvramescu <135025827+AlexiaAvramescu@users.noreply.github.com>
2025-03-30 23:21:55 +0300

    KEY-39 Implemented student list view

--- CatalogFrontend/catalog-frontend/src/App.js
+++ CatalogFrontend/catalog-frontend/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/Login';
+import TeacherClassView from './pages/TeacherClassView';
 
 function App() {
   return (
-    <Login/>
-    
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/class/:classId" element={<TeacherClassView />} />
+      </Routes>
+    </Router>
   );
 }
 
