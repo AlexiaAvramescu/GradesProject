b9af209cef704d6bc1f8fb2cc444cb516ed94d82
AlexTR22 <135030935+AlexTR22@users.noreply.github.com>
2025-04-04 15:37:56 +0300

    KEY-22 edited account ui page so that you can change your name

--- CatalogFrontend/catalog-frontend/src/pages/Account.js
+++ CatalogFrontend/catalog-frontend/src/pages/Account.js
@@ -1,25 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import '../css/Account.css';
 import { useSession } from '../context/sessionContext';
 
 function Account() {
 
-  //const [username, setUser] = useState("");
- // const [mail, setEmail] = useState("");
-  //const [isTeacher, setIsTeacher] = useState(false);
   const { user } = useSession();
+  const [isEditing, setIsEditing] = useState(false);
+  const [newName, setNewName] = useState(user.name);
+  const [showDialog,setShowDialog ] = useState(false);
 
-  
+  const handleSaveClick = async (e) => { };
+  const handleSavePassword = async (e) => { };
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
 
   return (
     <div className="account-container">
       <h2 className="account-title">Detalii Cont</h2>
       <div className="account-info">
-        <p><strong>Nume:</strong> {user.name}</p>
+        <p>
+          <strong>Nume:</strong>{" "}
+          {isEditing ? (
+            <input
+              type="text"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          ) : (
+            user.name
+          )}
+        </p>
         <p><strong>Email:</strong> {user.email}</p>
         <p><strong>Rol:</strong> {user.role === true ? "Profesor" : "Student"}</p>
       </div>
-      <button className="edit-button">Editează</button>
+      <div className="buttons">
+        {isEditing ? (
+          <button onClick={handleSaveClick} className="edit-button">
+            Salvează
+          </button>
+        ) : (
+          <button onClick={handleEditClick} className="edit-button">
+            Editează
+          </button>
+        )}
+        <button onClick={() => setShowDialog(true)} className="edit-button">
+            Editează parola
+          </button>
+          {showDialog && (
+        <ChangePasswordDialog
+          onSave={handleSavePassword}
+          onCancel={() => setShowDialog(false)}
+        />
+      )}
+      </div>
     </div>
 
   );
