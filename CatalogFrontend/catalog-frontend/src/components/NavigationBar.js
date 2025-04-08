1aaa03913d77cf5c6b219d38d83bc054b08ce1c7
AlexTR22 <135030935+AlexTR22@users.noreply.github.com>
2025-04-08 14:14:03 +0300

    KEY-48 added a button to logout, and function to delete the current session

--- CatalogFrontend/catalog-frontend/src/components/NavigationBar.js
+++ CatalogFrontend/catalog-frontend/src/components/NavigationBar.js
@@ -1,28 +1,52 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import "../css/NavBar.css";
+import { useSession } from '../context/sessionContext';
 
 function NavigationBar() {
-  const navigate = useNavigate();
-  const location = useLocation();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { user, logout } = useSession();
 
-  const goToAccount = () => {
-    navigate("/account");
-  };
+    const onclickLogo = () => {
+        if (!shouldHideButton) {
+            if (!user) {
+                navigate("/");
+            } else if (user.role) {
+                navigate("/teacher");
+            } else {
+                navigate("/student");
+            }
+        }
+    };
 
-  // Ascunde butonul pe prima pagină ("/")
-  const hideButtonOnPaths = ["/", "/login","/register"];
-  const shouldHideButton = hideButtonOnPaths.includes(location.pathname);
+    const goToAccount = () => {
+        navigate("/account");
+    };
 
-  return (
-    <nav className="nav-bar">
-      <h2 className="logo">Grading</h2>
-        {!shouldHideButton && (
-          <button className="action-btn" onClick={goToAccount}>
-            Account
-          </button>
-        )}
-    </nav>
-  );
+    const logoutFunc = () => {
+        logout();
+        navigate("/");
+    };
+
+    // Ascunde butonul pe prima pagină ("/")
+    const hideButtonOnPaths = ["/", "/login", "/register"];
+    const shouldHideButton = hideButtonOnPaths.includes(location.pathname);
+
+    return (
+        <nav className="nav-bar">
+            <h2 className="logo" onClick={onclickLogo}>Grading</h2>
+            {!shouldHideButton && (
+                <div className="nav-items">
+                    <button className="nav-action-btn" onClick={goToAccount}>
+                        Account
+                    </button>
+                    <button className="nav-action-btn" onClick={logoutFunc}>
+                        Logout
+                    </button>
+                </div>
+            )}
+        </nav>
+    );
 }
 
 export default NavigationBar;
